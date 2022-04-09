6c8b9afc4d39d176e5da11989389a985f3a3200e
Shashank <shashank445566@gmail.com>
2022-04-09 13:04:16 +0530

    tailwind css

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import 'tailwindcss/tailwind.css'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+function MyApp({ Component, pageProps }) {
+  return <>
+  <Navbar />
+  <Component {...pageProps} />
+  <Footer />
+
+  </>
+}
+export default MyApp
